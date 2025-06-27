68e1ab87010521a44b118ed84a4bddaccf7d9eeb
Carlos Piva <capj26@exemplo.com>
2025-06-27 09:03:46 -0300

    Primeiro commit do projeto AgroSens

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg'); // módulo para PostgreSQL
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // vem do Render
+  ssl: {
+    rejectUnauthorized: false // necessário para Supabase funcionar
+  }
+});
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ dbTime: result.rows[0].now });
+  } catch (err) {
+    console.error('Erro na conexão com o banco:', err);
+    res.status(500).send('Erro ao conectar ao banco');
+  }
+});
