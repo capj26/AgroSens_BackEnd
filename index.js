cc84150c618db7fad4d74d79353e9ed1a7854f67
Carlos Piva <capj26@exemplo.com>
2025-06-27 09:22:13 -0300

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const { Pool } = require('pg'); // módulo para PostgreSQL
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // vem do Render
-  ssl: {
-    rejectUnauthorized: false // necessário para Supabase funcionar
-  }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get('/', (req, res) => {
+  res.send('API AgroSens online');
 });
 
 app.get('/test-db', async (req, res) => {
@@ -12,7 +16,12 @@ app.get('/test-db', async (req, res) => {
     const result = await pool.query('SELECT NOW()');
     res.json({ dbTime: result.rows[0].now });
   } catch (err) {
-    console.error('Erro na conexão com o banco2:', err);
+    console.error(err);
     res.status(500).send('Erro ao conectar ao banco');
   }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
